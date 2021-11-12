76cbbb8b0fe7deeae31a3d4208e1bf06ab7359ae
Nick Sandison <nickstersand@gmail.com>
2021-11-12 11:37:43 -0600

    Created issue classes

---
+++ P5/FakeIssueRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6 {
+    class FakeIssueRepository : IIssueRepository {
+        public string NO_ERROR = "";
+        public string EMPTY_TITLE_ERROR = "A Title is required.";
+        public string EMPTY_DISCOVERY_DATETIME_ERROR = "Must select a Discovery Date/Time.";
+        public string FUTURE_DISCOVERY_DATETIME_ERROR = "Issues can't be from the future.";
+        private string EMPTY_DISCOVERER_ERROR = "A Discoverer is required.";
+        List<Issue> Issues;
+        public string Add(Issue issue) {
+            throw new NotImplementedException();
+        }
+
+        public List<Issue> GetAll(int ProjectId) {
+            throw new NotImplementedException();
+        }
+
+        public Issue GetIssueById(int Id) {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetIssuesByDiscoverer(int ProjectId) {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetIssuesByMonth(int ProjectId) {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalNumberOfIssues(int ProjectId) {
+            throw new NotImplementedException();
+        }
+
+        public string Modify(Issue issue) {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Issue issue) {
+            throw new NotImplementedException();
+        }
+
+        private string ValidateIssue(Issue issue) {
+            throw new NotImplementedException();
+        }
+
+        private bool IsDuplicate(string title) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ P5/IIssueRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace P6 {
+    interface IIssueRepository {
+        string Add(Issue issue);
+        List<Issue> GetAll(int ProjectId);
+        bool Remove(Issue issue);
+        string Modify(Issue issue);
+        int GetTotalNumberOfIssues(int ProjectId);
+        List<string> GetIssuesByMonth(int ProjectId);
+        List<string> GetIssuesByDiscoverer(int ProjectId);
+        Issue GetIssueById(int Id);
+    }
+}
