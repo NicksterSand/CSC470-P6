0e13fbc6fc1947482bd441f24e72082b1d6c6bd1
Nick Sandison <nickstersand@gmail.com>
2021-11-12 16:59:43 -0600

    Created Modify Issue Form

---
+++ P5/FormModifyIssue.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Builder {
+    partial class FormModifyIssue {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.textBoxInitialDescription = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.comboBoxStatus = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxComponent = new System.Windows.Forms.TextBox();
+            this.comboBoxDiscoverer = new System.Windows.Forms.ComboBox();
+            this.labelDiscoverer = new System.Windows.Forms.Label();
+            this.dateTimeDiscoveryDate = new System.Windows.Forms.DateTimePicker();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(263, 512);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(113, 32);
+            this.buttonCancel.TabIndex = 31;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Location = new System.Drawing.Point(402, 512);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(113, 32);
+            this.buttonModify.TabIndex = 30;
+            this.buttonModify.Text = "Modify Issue";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            // 
+            // textBoxInitialDescription
+            // 
+            this.textBoxInitialDescription.Location = new System.Drawing.Point(132, 230);
+            this.textBoxInitialDescription.Multiline = true;
+            this.textBoxInitialDescription.Name = "textBoxInitialDescription";
+            this.textBoxInitialDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInitialDescription.Size = new System.Drawing.Size(383, 260);
+            this.textBoxInitialDescription.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 17);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Initial Description";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(74, 171);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(52, 17);
+            this.labelStatus.TabIndex = 27;
+            this.labelStatus.Text = "Status:";
+            // 
+            // comboBoxStatus
+            // 
+            this.comboBoxStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxStatus.FormattingEnabled = true;
+            this.comboBoxStatus.Items.AddRange(new object[] {
+            "Open",
+            "Assigned",
+            "Fixed",
+            "Closed - Won\'t fix",
+            "Closed - Fixed",
+            "Closed - By design"});
+            this.comboBoxStatus.Location = new System.Drawing.Point(132, 171);
+            this.comboBoxStatus.Name = "comboBoxStatus";
+            this.comboBoxStatus.Size = new System.Drawing.Size(383, 24);
+            this.comboBoxStatus.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 17);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Component:";
+            // 
+            // textBoxComponent
+            // 
+            this.textBoxComponent.Location = new System.Drawing.Point(132, 142);
+            this.textBoxComponent.Name = "textBoxComponent";
+            this.textBoxComponent.Size = new System.Drawing.Size(383, 22);
+            this.textBoxComponent.TabIndex = 24;
+            // 
+            // comboBoxDiscoverer
+            // 
+            this.comboBoxDiscoverer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDiscoverer.FormattingEnabled = true;
+            this.comboBoxDiscoverer.Location = new System.Drawing.Point(132, 111);
+            this.comboBoxDiscoverer.Name = "comboBoxDiscoverer";
+            this.comboBoxDiscoverer.Size = new System.Drawing.Size(383, 24);
+            this.comboBoxDiscoverer.TabIndex = 23;
+            // 
+            // labelDiscoverer
+            // 
+            this.labelDiscoverer.AutoSize = true;
+            this.labelDiscoverer.Location = new System.Drawing.Point(46, 111);
+            this.labelDiscoverer.Name = "labelDiscoverer";
+            this.labelDiscoverer.Size = new System.Drawing.Size(80, 17);
+            this.labelDiscoverer.TabIndex = 22;
+            this.labelDiscoverer.Text = "Discoverer:";
+            // 
+            // dateTimeDiscoveryDate
+            // 
+            this.dateTimeDiscoveryDate.Location = new System.Drawing.Point(132, 83);
+            this.dateTimeDiscoveryDate.Name = "dateTimeDiscoveryDate";
+            this.dateTimeDiscoveryDate.Size = new System.Drawing.Size(383, 22);
+            this.dateTimeDiscoveryDate.TabIndex = 21;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(132, 55);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(383, 22);
+            this.textBoxTitle.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Id:";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Enabled = false;
+            this.textBoxId.Location = new System.Drawing.Point(132, 26);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.ReadOnly = true;
+            this.textBoxId.Size = new System.Drawing.Size(47, 22);
+            this.textBoxId.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Title:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Discovery Date:";
+            // 
+            // FormModifyIssue
+            // 
+            this.AcceptButton = this.buttonModify;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(560, 584);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.textBoxInitialDescription);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.comboBoxStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxComponent);
+            this.Controls.Add(this.comboBoxDiscoverer);
+            this.Controls.Add(this.labelDiscoverer);
+            this.Controls.Add(this.dateTimeDiscoveryDate);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormModifyIssue";
+            this.Text = "Modify Issue";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.TextBox textBoxInitialDescription;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ComboBox comboBoxStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxComponent;
+        private System.Windows.Forms.ComboBox comboBoxDiscoverer;
+        private System.Windows.Forms.Label labelDiscoverer;
+        private System.Windows.Forms.DateTimePicker dateTimeDiscoveryDate;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
