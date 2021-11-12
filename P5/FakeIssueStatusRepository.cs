b7a437dbb1cfe00412cd7d53e30a3e8bb5aad3d7
Nick Sandison <nickstersand@gmail.com>
2021-11-12 13:50:13 -0600

    Created issue status classes

---
+++ P5/FakeIssueStatusRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6 {
+    class FakeIssueStatusRepository : IIssueStatusRepository {
+        private List<IssueStatus> IssueStatuses;
+        public void Add(int Id, string value) {
+            throw new NotImplementedException();
+        }
+
+        public List<IssueStatus> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public int GetIdByStatus(string value) {
+            throw new NotImplementedException();
+        }
+
+        public string GetValueById(int Id) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ P5/IssueStatus.cs
@@ -0,0 +1,6 @@
+﻿namespace P6 {
+    class IssueStatus {
+        public int Id;
+        public string Value;
+    }
+}
